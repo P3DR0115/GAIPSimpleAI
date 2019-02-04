f1fd321488f699cb7ccdb7460f4765c72e423a9e
Pedro A. Tresgallo <P3dro115@gmail.com>
2019-02-03 23:46:38 -0600

    Add project files.

---
+++ GAIPSimpleAI/Game.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAIPSimpleAI
+{
+    public class Game
+    {
+        public Agent smith;
+        public static Map puzzleMap;
+
+        public Game()
+        {
+            puzzleMap = new Map();
+            setPuzzleMap();
+            smith = new Agent();
+
+            //smith.XGoal = smith.YGoal = 9;
+            smith.play();
+        }
+
+        public void setPuzzleMap()
+        {
+            for (int x = 1; x <= 10; x++)
+            {
+                for (int y = 1; y <= 10; y++)
+                {
+                    puzzleMap.mapArray[x, y] = mapSpaceState.movable;
+                }
+            }
+
+            puzzleMap.mapArray[2, 3] = mapSpaceState.immovable;
+            puzzleMap.mapArray[3, 6] = mapSpaceState.immovable;
+            puzzleMap.mapArray[3, 8] = mapSpaceState.immovable;
+            puzzleMap.mapArray[4, 2] = mapSpaceState.immovable;
+            puzzleMap.mapArray[4, 3] = mapSpaceState.immovable;
+            puzzleMap.mapArray[4, 4] = mapSpaceState.immovable;
+            puzzleMap.mapArray[5, 8] = mapSpaceState.immovable;
+            puzzleMap.mapArray[5, 9] = mapSpaceState.immovable;
+            puzzleMap.mapArray[5, 10] = mapSpaceState.immovable;
+            puzzleMap.mapArray[6, 1] = mapSpaceState.immovable;
+            puzzleMap.mapArray[6, 2] = mapSpaceState.immovable;
+            puzzleMap.mapArray[6, 3] = mapSpaceState.immovable;
+            puzzleMap.mapArray[7, 6] = mapSpaceState.immovable;
+            puzzleMap.mapArray[7, 8] = mapSpaceState.immovable;
+            puzzleMap.mapArray[9, 2] = mapSpaceState.immovable;
+            puzzleMap.mapArray[9, 3] = mapSpaceState.immovable;
+            puzzleMap.mapArray[9, 4] = mapSpaceState.immovable;
+            puzzleMap.mapArray[9, 5] = mapSpaceState.immovable;
+            puzzleMap.mapArray[9, 8] = mapSpaceState.immovable;
+            puzzleMap.mapArray[9, 9] = mapSpaceState.immovable;
+            puzzleMap.mapArray[9, 10] = mapSpaceState.immovable;
+        }
+
+        public static mapSpaceState checkLocation(int xCoord, int yCoord)
+        {
+            return puzzleMap.mapArray[xCoord, yCoord];
+        }
+    }
+
+}
---
+++ GAIPSimpleAI/Map.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAIPSimpleAI
+{
+    public enum mapSpaceState { unknown, movable, immovable, visitedOnce };
+
+    public class Map
+    {
+        public mapSpaceState[,] mapArray = new mapSpaceState[12, 12];
+
+        public Map()
+        {
+            for (int x = 0; x <= 11; x++)
+            {
+                for (int y = 0; y <= 11; y++)
+                {
+                    mapArray[x, y] = mapSpaceState.unknown;
+
+                    if (x == 0 || y == 0 || x == 11 || y == 11)
+                    {
+                        mapArray[x, y] = mapSpaceState.immovable;
+                    }
+                }
+            }
+            
+
+            //setMap();
+
+        }
+
+
+    }
+}
